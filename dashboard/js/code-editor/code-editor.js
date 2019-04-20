a61473d58d0aad2046b978684a5efa990a67d305
Ryan Nguyen <ryan@Ryans-MacBook-Pro-2.local>
2019-04-20 10:41:11 -0600

    remove some stuff change some stuff

--- dashboard/js/code-editor/code-editor.js
+++ dashboard/js/code-editor/code-editor.js
@@ -1,5 +1,4 @@
-// CodeMirror, copyright (c) by Marijn Haverbeke and others
-// Distributed under an MIT license: http://codemirror.net/LICENSE
+
 
 // TODO actually recognize syntax of TypeScript constructs
 
