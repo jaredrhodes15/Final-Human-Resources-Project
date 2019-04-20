91f36d2c5f5b9b863426974e292854f0efda8624
Jared Rhodes <jaredrhodes15@gmail.com>
2019-04-20 10:41:44 -0600

    twilio working to send to one number and masked by dot env

--- send_sms.js
+++ send_sms.js
@@ -1,13 +1,15 @@
+require('dotenv').config()
 // Download the helper library from https://www.twilio.com/docs/node/install
 // Your Account Sid and Auth Token from twilio.com/console
 // DANGER! This is insecure. See http://twil.io/secure
-const client = require('twilio')(process.env.TWILIO_ACCOUNT_SID, process.env.TWILIO_TOKEN);
+const client = require('twilio')(process.env.TWILIO_ACCOUNT_SID, process.env.TWILIO_AUTH_TOKEN);
 console.log(process.env.TWILIO_ACCOUNT_SID);
-console.log(process.env.TWILIO_TOKEN);
+console.log(process.env.TWILIO_AUTH_TOKEN);
 client.messages
   .create({
      body: 'This is the ship that made the Kessel Run in fourteen parsecs?',
-     from: process.env.ADMIN_PHONE,
+     from: process.env.TWILIO_NUMBER,
      to: process.env.OUTGOING_NUMBERS
    })
   .then(message => console.log(message.sid));
+
