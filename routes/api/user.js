c92e999d84355eda18e2c67db852a51132f8b1ce
applegrubber <jscottatwood@gmail.com>
2019-04-24 19:31:05 -0600

    made changes to Database, but still getting a 400 bad request error. Working the problem

--- routes/api/user.js
+++ routes/api/user.js
@@ -2,12 +2,18 @@ const router = require("express").Router();
 const addUser = require("../../controllers/UserController");
 
 router.route("/")
-    .get(addUser.findAll)
+    .get(addUser.findAll);
+
+router.route("/api/create")
     .post(addUser.createUser);
 
-router.route("/api/user")
-    // .get(addUser.findById)
-    .put(addUser.updateUser)
+router.route("/api/:id")
+    .get(addUser.findById)
+
+router.route("/api/update")
+    .put(addUser.updateUser);
+
+routere.route("/api/delete")
     .delete(addUser.deleteUser);
 
 module.exports = router;
