38f26d0e5c8a4eca0992c31b74ff93053f2942a8
applegrubber <jscottatwood@gmail.com>
2019-04-24 19:02:11 -0600

    bit of work on the database that was deleted

--- routes/api/user.js
+++ routes/api/user.js
@@ -2,6 +2,12 @@ const router = require("express").Router();
 const addUser = require("../../controllers/UserController");
 
 router.route("/")
-    .get(addUser.findAll);
+    .get(addUser.findAll)
+    .post(addUser.createUser);
+
+router.route("/api/user")
+    // .get(addUser.findById)
+    .put(addUser.updateUser)
+    .delete(addUser.deleteUser);
 
 module.exports = router;
